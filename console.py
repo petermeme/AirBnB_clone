edb4b165037cdb5dc3c52af370c394124751282c
John Gaitho <johngaitho05@gmail.com>
2023-10-11 10:06:26 +0300

    refactor

--- console.py
+++ console.py
@@ -30,17 +30,13 @@ class HBNBCommand(cmd.Cmd):
     def implemented_models(self):
         return ['BaseModel']
 
-    @staticmethod
-    def print_error(msg):
-        print(msg)
-
     def do_create(self, arg):
         """Creates an instance of the passed class if it exists,
         saves it and prints its id"""
         if not arg:
-            return self.print_error("** class name missing **")
+            return print("** class name missing **")
         if arg not in self.implemented_models:
-            return self.print_error("** class doesn't exist **")
+            return print("** class doesn't exist **")
         obj = eval(arg)()
         obj.save()
         print(obj.id)
@@ -48,39 +44,39 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         """prints the string representation of an instance"""
         if not arg:
-            return self.print_error("** class name missing **")
+            return print("** class name missing **")
         args = arg.split()
         if args[0] not in self.implemented_models:
-            return self.print_error("** class doesn't exist **")
+            return print("** class doesn't exist **")
         if len(args) < 2:
-            return self.print_error("** instance id missing **")
+            return print("** instance id missing **")
         key = "{}.{}".format(args[0], args[1])
         instance = storage.all().get(key)
         if not instance:
-            return self.print_error("** no instance found **")
+            return print("** no instance found **")
         print(instance)
 
     def do_destroy(self, arg):
         """deletes an instance given a classname and instance id"""
         if not arg:
-            return self.print_error("** class name missing **")
+            return print("** class name missing **")
         args = arg.split()
         if args[0] not in self.implemented_models:
-            return self.print_error("** class doesn't exist **")
+            return print("** class doesn't exist **")
         if len(args) < 2:
-            return self.print_error("** instance id missing **")
+            return print("** instance id missing **")
         key = "{}.{}".format(args[0], args[1])
         instance = storage.all().get(key)
         if not instance:
-            return self.print_error("** no instance found **")
+            return print("** no instance found **")
         storage.delete(key)
 
     def do_all(self, arg):
         """prints all instances of a given model"""
         if not arg:
-            return self.print_error("** class name missing **")
+            return print("** class name missing **")
         if arg not in self.implemented_models:
-            return self.print_error("** class doesn't exist **")
+            return print("** class doesn't exist **")
         instances = [str(instance) for k, instance in storage.all().items() if
                      k.startswith("{}.".format(arg))]
         print(instances)
@@ -88,20 +84,20 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, arg):
         """updates a model instance"""
         if not arg:
-            return self.print_error("** class name missing **")
+            return print("** class name missing **")
         args = arg.split()
         if args[0] not in self.implemented_models:
-            return self.print_error("** class doesn't exist **")
+            return print("** class doesn't exist **")
         if len(args) < 2:
-            return self.print_error("** instance id missing **")
+            return print("** instance id missing **")
         key = "{}.{}".format(args[0], args[1])
         instance = storage.all().get(key)
         if not instance:
-            return self.print_error("** no instance found **")
+            return print("** no instance found **")
         if len(args) < 3:
-            return self.print_error("** attribute name missing **")
+            return print("** attribute name missing **")
         if len(args) < 4:
-            return self.print_error("** value missing **")
+            return print("** value missing **")
         setattr(instance, args[2], eval(args[3]))
         instance.save()
 
