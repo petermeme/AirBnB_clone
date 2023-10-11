cedb98b4085192ac4ea311ea2bcaa91778732bf5
petermeme <petermeme90@gmail.com>
2023-10-11 17:35:14 +0300

    airbnb_clone

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Defines the States."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Attributes:
+    name (str): The name of the state.
+    """
+
+    name = ""
