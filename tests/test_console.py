d78e68a952a1d023c12be3f6248ea3e16e65203e
John Gaitho <johngaitho05@gmail.com>
2023-10-13 12:59:55 +0300

    test show

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is a Testsuite for the console"""
 import json
+import uuid
 from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
@@ -15,37 +16,41 @@ class TestConsole(TestCase):
     file_name = "file.json"
 
     def setUp(self):
-        pass
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create User")
+            self.user_id = f.getvalue().strip().split('\n')[-1]
+            HBNBCommand().onecmd("create State")
+            self.state_id = f.getvalue().strip().split('\n')[-1]
+            HBNBCommand().onecmd("create Review")
+            self.review_id = f.getvalue().strip().split('\n')[-1]
+            HBNBCommand().onecmd("create Place")
+            self.place_id = f.getvalue().strip().split('\n')[-1]
+            HBNBCommand().onecmd("create City")
+            self.city_id = f.getvalue().strip().split('\n')[-1]
+            HBNBCommand().onecmd("create BaseModel")
+            self.base_model_id = f.getvalue().strip().split('\n')[-1]
+            HBNBCommand().onecmd("create Amenity")
+            self.amenity_id = f.getvalue().strip().split('\n')[-1]
 
     def test_create_class_missing(self):
         """Tests creation of BaseModel instance"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create")
-            uid = f.getvalue().strip()
+            uid = f.getvalue().strip().split('\n')[-1]
             self.assertEqual(uid, "** class name missing **")
 
     def test_create_class_not_exist(self):
         """Tests creation of BaseModel instance"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create Goat")
-            uid = f.getvalue().strip()
+            uid = f.getvalue().strip().split('\n')[-1]
             self.assertEqual(uid, "** class doesn't exist **")
-
-    def test_create_base_model(self):
-        """Tests creation of BaseModel instance"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create BaseModel")
-            uid = f.getvalue().strip()
-            self.assertTrue(len(uid) == 36)  # a uuid4 string
-            with open(self.file_name, 'r') as file:
-                keys = json.loads(file.read()).keys()
-                self.assertIn('BaseModel.{}'.format(uid), keys)
 
     def test_create_user(self):
         """Tests creation of User instance"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create User")
-            uid = f.getvalue().strip()
+            uid = f.getvalue().strip().split('\n')[-1]
             self.assertTrue(len(uid) == 36)  # a uuid4 string
             with open(self.file_name, 'r') as file:
                 keys = json.loads(file.read()).keys()
@@ -55,49 +60,135 @@ class TestConsole(TestCase):
         """Tests creation of State instance"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create State")
-            uid = f.getvalue().strip()
+            uid = f.getvalue().strip().split('\n')[-1]
             self.assertTrue(len(uid) == 36)  # a uuid4 string
             with open(self.file_name, 'r') as file:
                 keys = json.loads(file.read()).keys()
                 self.assertIn('State.{}'.format(uid), keys)
 
-    def test_create_city(self):
-        """Tests creation of City instance"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create City")
-            uid = f.getvalue().strip()
-            self.assertTrue(len(uid) == 36)  # a uuid4 string
-            with open(self.file_name, 'r') as file:
-                keys = json.loads(file.read()).keys()
-                self.assertIn('City.{}'.format(uid), keys)
-
-    def test_create_place(self):
-        """Tests creation of Place instance"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create Place")
-            uid = f.getvalue().strip()
-            self.assertTrue(len(uid) == 36)  # a uuid4 string
-            with open(self.file_name, 'r') as file:
-                keys = json.loads(file.read()).keys()
-                self.assertIn('Place.{}'.format(uid), keys)
-
-    def test_create_amenity(self):
-        """Tests creation of Amenity instance"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create Amenity")
-            uid = f.getvalue().strip()
-            self.assertTrue(len(uid) == 36)  # a uuid4 string
-            with open(self.file_name, 'r') as file:
-                keys = json.loads(file.read()).keys()
-                self.assertIn('Amenity.{}'.format(uid), keys)
-
     def test_create_review(self):
         """Tests creation of City instance"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create Review")
-            uid = f.getvalue().strip()
+            uid = f.getvalue().strip().split('\n')[-1]
             self.assertTrue(len(uid) == 36)  # a uuid4 string
             with open(self.file_name, 'r') as file:
                 keys = json.loads(file.read()).keys()
                 self.assertIn('Review.{}'.format(uid), keys)
 
+    def test_create_place(self):
+        """Tests creation of Place instance"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create Place")
+            uid = f.getvalue().strip().split('\n')[-1]
+            self.assertTrue(len(uid) == 36)  # a uuid4 string
+            with open(self.file_name, 'r') as file:
+                keys = json.loads(file.read()).keys()
+                self.assertIn('Place.{}'.format(uid), keys)
+
+    def test_create_city(self):
+        """Tests creation of City instance"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create City")
+            uid = f.getvalue().strip().split('\n')[-1]
+            self.assertTrue(len(uid) == 36)  # a uuid4 string
+            with open(self.file_name, 'r') as file:
+                keys = json.loads(file.read()).keys()
+                self.assertIn('City.{}'.format(uid), keys)
+
+    def test_create_base_model(self):
+        """Tests creation of BaseModel instance"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            uid = f.getvalue().strip().split('\n')[-1]
+            self.assertTrue(len(uid) == 36)  # a uuid4 string
+            with open(self.file_name, 'r') as file:
+                keys = json.loads(file.read()).keys()
+                self.assertIn('BaseModel.{}'.format(uid), keys)
+
+    def test_create_amenity(self):
+        """Tests creation of Amenity instance"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create Amenity")
+            uid = f.getvalue().strip().split('\n')[-1]
+            self.assertTrue(len(uid) == 36)  # a uuid4 string
+            with open(self.file_name, 'r') as file:
+                keys = json.loads(file.read()).keys()
+                self.assertIn('Amenity.{}'.format(uid), keys)
+
+    def test_show_class_missing(self):
+        """Tests creation of BaseModel instance"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show")
+            uid = f.getvalue().strip().split('\n')[-1]
+            self.assertEqual(uid, "** class name missing **")
+
+    def test_show_class_not_exist(self):
+        """Tests creation of BaseModel instance"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show Goat")
+            uid = f.getvalue().strip().split('\n')[-1]
+            self.assertEqual(uid, "** class doesn't exist **")
+
+    def test_show_uid_not_exist(self):
+        """Tests creation of BaseModel instance"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show Goat %s" % str(uuid.uuid4()))
+            uid = f.getvalue().strip().split('\n')[-1]
+            self.assertEqual(uid, "** class doesn't exist **")
+
+    def test_show_user(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show User %s" % self.user_id)
+            res = f.getvalue().strip().split('\n')[-1]
+            key = 'User.{}'.format(self.user_id)
+            expected = str(storage.all().get(key))
+            self.assertEqual(res, expected)
+
+    def test_show_state(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show State %s" % self.state_id)
+            res = f.getvalue().strip().split('\n')[-1]
+            key = 'State.{}'.format(self.state_id)
+            expected = str(storage.all().get(key))
+            self.assertEqual(res, expected)
+
+    def test_show_review(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show Review %s" % self.review_id)
+            res = f.getvalue().strip().split('\n')[-1]
+            key = 'Review.{}'.format(self.review_id)
+            expected = str(storage.all().get(key))
+            self.assertEqual(res, expected)
+
+    def test_show_place(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show Place %s" % self.place_id)
+            res = f.getvalue().strip().split('\n')[-1]
+            key = 'Place.{}'.format(self.place_id)
+            expected = str(storage.all().get(key))
+            self.assertEqual(res, expected)
+
+    def test_show_city(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show City %s" % self.city_id)
+            res = f.getvalue().strip().split('\n')[-1]
+            key = 'City.{}'.format(self.city_id)
+            expected = str(storage.all().get(key))
+            self.assertEqual(res, expected)
+
+    def test_show_base_model(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show BaseModel %s" % self.base_model_id)
+            res = f.getvalue().strip().split('\n')[-1]
+            key = 'BaseModel.{}'.format(self.base_model_id)
+            expected = str(storage.all().get(key))
+            self.assertEqual(res, expected)
+
+    def test_show_amenity(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show Amenity %s" % self.amenity_id)
+            res = f.getvalue().strip().split('\n')[-1]
+            key = 'Amenity.{}'.format(self.amenity_id)
+            expected = str(storage.all().get(key))
+            self.assertEqual(res, expected)
